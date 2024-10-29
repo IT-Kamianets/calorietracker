6cfb31512a3dfc9215fe227bf9c4a5a631b58f2c
serhio <119081306+xanax4rl@users.noreply.github.com>
2024-10-29 12:40:35 +0000

    new version app calorietracker 1.01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,54 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';  // <<<< import it here
+import { CommonModule } from '@angular/common'; // Додайте цей імпорт
+import { products } from './array';
+
+export class AppModule { }
+
+interface Product{
+  name: string;
+  calories: number;
+  protein: number;
+  fat: number;
+  carbs: number;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  readonly foodList:Product[]= products;
+
   title = 'calorietracker';
+  searchText: string = '';
+  selectedFood: any = null;
+
+  filteredFoodList = [...this.foodList];
+
+  filterFoods() {
+    this.filteredFoodList = this.foodList.filter(food =>
+      food.name.toLowerCase().includes(this.searchText.toLowerCase()) ||
+      food.calories.toString().includes(this.searchText)
+    );
+  }
+
+  showFoodDetails(food: any) {
+    this.selectedFood = food;
+  }
+
+  closeDetails() {
+    this.selectedFood = null;
+  }
 }
+
+
+
+
+  
+
+
